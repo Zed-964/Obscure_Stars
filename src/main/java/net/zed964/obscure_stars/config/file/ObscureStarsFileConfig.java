743125326963bbb19dc2d31a4c5e3962627eaa4d
Zed-964 <enzotechnic@gmail.com>
2024-07-03 14:53:54 +0200

    feat(suffocation) : Ajout d'un fichier de conf pour le paramétrage du mod pour connaitre la liste des dimensions ou l'effet suffocation sera présent

---
+++ src/main/java/net/zed964/obscure_stars/config/file/ObscureStarsFileConfig.java
@@ -0,0 +1,45 @@
+package net.zed964.obscure_stars.config.file;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.zed964.obscure_stars.constants.DimensionsConstants;
+
+import java.util.List;
+
+/**
+ * Class pour gérer le fichier de conf du mod
+ */
+public class ObscureStarsFileConfig {
+
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    public static final ForgeConfigSpec SPEC;
+
+    public static final String FILE_NAME_CONFIG = "obscure-stars-config.toml";
+
+    public static final ForgeConfigSpec.ConfigValue<List<String>> DIMENSIONS_HAS_SUFFOCATION_EFFECT;
+
+    /**
+     * Constructeur privé par défaut
+     */
+    private ObscureStarsFileConfig(){
+
+    }
+
+    static {
+        BUILDER.push("Configs for Obscure Stars !");
+
+        BUILDER.comment("This file is used to configure the mod Obscure Stars");
+
+        BUILDER.pop();
+
+        BUILDER.push("Effect Configs : ");
+
+        DIMENSIONS_HAS_SUFFOCATION_EFFECT = BUILDER.comment("""
+                Here, you can add all the dimensions you want to have the suffocation effect in.
+                    Warning : you need to add the name of the dimension with his namespace i.e. "minecraft:the_end"
+                """).define("List of dimensions that have the suffocation effect", List.of(DimensionsConstants.ASTEROID_FIELD_DIMENSION, "minecraft:the_end"));
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
--- src/main/java/net/zed964/obscure_stars/constants/DimensionsConstants.java
+++ src/main/java/net/zed964/obscure_stars/constants/DimensionsConstants.java
@@ -5,7 +5,11 @@ package net.zed964.obscure_stars.constants;
  */
 public class DimensionsConstants {
 
+    public static final String DIMENSION_NAMESPACE = "obscure_stars";
+
     public static final String ASTEROID_FIELD_PATH = "asteroid_field";
+
+    public static final String ASTEROID_FIELD_DIMENSION = DIMENSION_NAMESPACE + ":" + ASTEROID_FIELD_PATH;
 
     /**
      * Constructeur privé par défault
